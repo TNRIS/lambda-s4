b8911452ab0fc22a777b3ced4fa49bbca1c53908
Brez <smudgeservices@gmail.com>
2018-06-29 14:38:50 -0500

    init ls4 04 mapfile

---
+++ ls4-05-mapfile/lambda_function.py
@@ -0,0 +1,95 @@
+# --------------- IMPORTS ---------------
+import os
+import boto3
+import psycopg2
+
+# Database Connection Info
+database = os.environ.get('DB_NAME')
+username = os.environ.get('DB_USER')
+password = os.environ.get('DB_PASSWORD')
+host = os.environ.get('DB_HOST')
+port = os.environ.get('DB_PORT')
+mapserver_access_key = os.environ.get('MAPSERVER_ACCESS_KEY_ID')
+mapserver_secret_access_key = os.environ.get('MAPSERVER_SECRET_ACCESS_KEY')
+
+conn_string = "dbname='%s' user='%s' host='%s' password='%s'" % (database, username, host, password)
+conn = psycopg2.connect(conn_string)
+cur = conn.cursor()
+
+# --------------- Main handler ------------------
+
+def lambda_handler(event, context):
+    print(event)
+    print(context)
+    # establish some variables
+    source_bucket = event['sourceBucket']
+    source_key = event['sourceKey']
+
+    # verify input is a cog
+    if 'cog/tile_index/' not in source_key:
+        print("source key is not a cog tile index! exiting...")
+        print(source_key)
+        return
+
+    filename = source_key.split("/")[-1]
+    collection = filename.replace('.shp', '')
+    print("collection: %s" % collection)
+    mapname = collection.upper().replace("_", "")
+    print("mapname: %s" % mapname)
+    wmstitle = collection.replace('_', ' ').title()
+    print("wmstitle: %s" % collection)
+    tablename = collection.lower()
+    print("tablename: %s" % tablename)
+
+    extent_query = "SELECT ST_Extent(geom) FROM %s;" % tablename
+    print(extent_query)
+    cur.execute(extent_query)
+    response = cur.fetchone()
+    box_string = response[0]
+    print(response)
+    coords_string = box_string[(box_string.find('(') + 1):box_string.find(')')]
+    mins = coords_string.split(",")[0]
+    maxs = coords_string.split(",")[1]
+    xmin = str(int(float(mins.split(" ")[0])) - 30)
+    ymin = str(int(float(mins.split(" ")[1])) - 30)
+    xmax = str(int(float(maxs.split(" ")[0])) + 30)
+    ymax = str(int(float(maxs.split(" ")[1])) + 30)
+    print("extent coordinates found!")
+    print(xmin, ymin, xmax, ymax)
+
+    with open('template.map') as template:
+        t = template.read()
+    print('template opened')
+
+    mapfile = tablename + '.map'
+    print('writing %s' % mapfile)
+    with open('/tmp/' + mapfile, 'w') as m:
+        t = t.replace('<database>', database)
+        t = t.replace('<username>', username)
+        t = t.replace('<password>', password)
+        t = t.replace('<host>', host)
+        t = t.replace('<port>', port)
+        t = t.replace('<mapserver_access_key>', mapserver_access_key)
+        t = t.replace('<mapserver_secret_access_key>', mapserver_secret_access_key)
+        t = t.replace('<collection>', collection)
+        t = t.replace('<mapname>', mapname)
+        t = t.replace('<wmstitle>', wmstitle)
+        t = t.replace('<tablename>', tablename)
+        t = t.replace('<xmin>', xmin)
+        t = t.replace('<ymin>', ymin)
+        t = t.replace('<xmax>', xmax)
+        t = t.replace('<ymax>', ymax)
+        m.write(t)
+    print('mapfile written')
+
+    # connect to s3 and upload mapfile
+    upload_key = 'mapfiles/%s' % mapfile
+    s3 = boto3.resource('s3')
+    s3.Bucket(source_bucket).upload_file('/tmp/' + mapfile,upload_key,ExtraArgs={'Metadata':{'mode':'33204','uid':'500','gid':'500','mtime':'1528814551'}})
+    print("upload success!")
+
+    os.remove('/tmp/' + mapfile)
+    print("local mapfile deleted. /tmp clean.")
+
+if __name__ == '__main__':
+    lambda_handler(event='event', context='context')
