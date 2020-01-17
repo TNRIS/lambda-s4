241a4da8ab1b9d2970006a12594ce291c9012f06
brez <adam.breznicky@twdb.texas.gov>
2020-01-17 07:40:41 -0600

    qc scripts (#9)
    * comparison stats
    
    * print statements for clean qc read
    
    * identify ls4 scanned indexes not georeferenced in Q
    
    * identify double extensions
    
    * comment out loops

--- sandbox/q_georef_identify.py
+++ sandbox/q_georef_identify.py
@@ -1,6 +1,4 @@
 import boto3
-# import requests
-# import json
 
 client = boto3.client('s3')
 ls4_bucket = 'tnris-ls4'
@@ -10,12 +8,11 @@ q_bucket = 'tnris-public-data'
 if q_bucket != '' and ls4_bucket != '':
     ls4_tifs = []
     ls4_deets = []
+    q_index_keys = []
     q_tifs = []
-    q_deets = []
-    tiftif = []
 
     def ls4_run(ct=None, loop=0):
-        # print('loop: ' + str(loop))
+        print('loop: ' + str(loop))
         if ct is None:
             response = client.list_objects_v2(
                 Bucket=ls4_bucket,
@@ -32,7 +29,7 @@ if q_bucket != '' and ls4_bucket != '':
                     c['Key'][-4:] == '.tif' or
                     c['Key'][-4:] == '.TIF'):
                 ls4_tifs.append(c['Key'])
-
+            
                 county = c['Key'].split('/')[1]
                 filename = c['Key'].split('/')[-1].replace('.tif', '').replace('.TIF', '')
                 agency = filename.split('_')[0]
@@ -51,7 +48,7 @@ if q_bucket != '' and ls4_bucket != '':
             return 
 
     def q_run(ct=None, loop=0):
-        # print('loop: ' + str(loop))
+        print('loop: ' + str(loop))
         if ct is None:
             response = client.list_objects_v2(
                 Bucket=q_bucket,
@@ -64,32 +61,16 @@ if q_bucket != '' and ls4_bucket != '':
                 ContinuationToken=ct
             )
         for c in response['Contents']:
-            if (
-                '.tif.tif' in c['Key'] or
-                '.tif.TIF' in c['Key'] or
-                '.TIF.tif' in c['Key'] or
-                '.TIF.TIF' in c['Key']
-            ):
-                tiftif.append(c['Key'])
             if '/Index/' in c['Key']:
                 # find all tifs that are not in the MultiCounty subdirectory
                 # and are not AMS (since AMS are mainly line indexes)
                 # and are not line indexes (LI) at all
+                q_index_keys.append(c['Key'])
                 if (
                     c['Key'][-4:] == '.tif'
                     or c['Key'][-4:] == '.TIF'
                     ) and 'MultiCounty' not in c['Key'] and 'AMS' not in c['Key'] and '_LI_' not in c['Key']:
                     q_tifs.append(c['Key'])
-                    try:
-                        county = c['Key'].split('/')[2]
-                        filename = c['Key'].split('/')[-1].replace('.tif', '').replace('.TIF', '')
-                        agency = filename.split('_')[0]
-                        year = filename.split('_')[1]
-                        sheet = filename.split('_')[2]
-                        q_deets.append([county, agency, year, sheet])
-                        # print(county, agency, year, sheet)
-                    except:
-                        print('issue getting county, agency, year, & sheet:' + c['Key'])
                 if c['Key'][-4:] == '.TIF' or c['Key'][-4:] == '.TIFF' or c['Key'][-4:] == '.tiff':
                     print('what the TIF???' + c['Key'])
                 
@@ -109,27 +90,72 @@ if q_bucket != '' and ls4_bucket != '':
     print('ls4: %s --- q: %s' % (str(len(ls4_tifs)), str(len(q_tifs))))
 
     print('time to compare....')
-    print('ls4 tifs not in Q drive:')
-    count = 0
-    for l in ls4_deets:
-        if l not in q_deets:
-            count += 1
-            print(l)
-    print('%s total ls4 tifs not in Q drive.' % str(count))
-    print('q tifs not in ls4:')
-    count = 0
-    for q in q_deets:
-        if q not in ls4_deets:
-            count += 1
-            print(q)
-    print('%s q tifs not in ls4.' % str(count))
+    wf = []
+    af = []
+    ov = []
+    counter = 1
+    for t in q_tifs:
+        # print('%s/%s' % (str(counter), str(len(q_tifs))))
+        worldfile = t.replace('.tif', '.tfwx')
+        if worldfile not in q_index_keys:
+            # print('missing worldfile: ' + t)
+            wf.append(t)
+        
+        auxfile = t + '.aux.xml'
+        if auxfile not in q_index_keys:
+            # print('missing auxfile: ' + t)
+            af.append(t)
 
-    print('Q drive %s double extensions---' % str(len(tiftif)))
-    for tt in tiftif:
-        print(tt)
+        overviews = t + '.ovr'
+        if overviews not in q_index_keys:
+            # print('missing overviews: ' + t)
+            ov.append(t)
+
+        counter += 1
+    print('missing Q georef totals---')
+    print('worldfiles/auxfiles/overviews')
+    print('%s/%s/%s' % (str(len(wf)), str(len(af)), str(len(ov))))
+    
+    ls4_not_georef = []
+    wf = []
+    af = []
+    ov = []
+    counter = 1
+    for d in ls4_deets:
+        # print('%s/%s' % (str(counter), str(len(ls4_deets))))
+        keypath = 'prod-historic/Historic_Images/%s/Index/%s_%s_%s.tif' % (d[0], d[1], d[2], d[3])
+        worldfile = keypath.replace('.tif', '.tfwx')
+        if worldfile not in q_index_keys:
+            # print('missing worldfile: ' + keypath)
+            wf.append(keypath)
+        
+        auxfile = keypath + '.aux.xml'
+        if auxfile not in q_index_keys:
+            # print('missing auxfile: ' + keypath)
+            af.append(keypath)
+
+        overviews = keypath + '.ovr'
+        if overviews not in q_index_keys:
+            # print('missing overviews: ' + keypath)
+            ov.append(keypath)
+
+        if (
+            worldfile not in q_index_keys or
+            auxfile not in q_index_keys or
+            overviews not in q_index_keys):
+            ls4_not_georef.append(d)
+
+        counter += 1
+    print('LS4 keys missing Q georef totals---')
+    print('worldfiles/auxfiles/overviews')
+    print('%s/%s/%s' % (str(len(wf)), str(len(af)), str(len(ov))))
+    print('ls4 scanned indexes missing 1(or more) georeference files in q---')
+    print('total count: %s' % str(len(ls4_not_georef)))
+    for l in ls4_not_georef:
+        print(l)
 
 else:
-    print('buckets not declared. populate variables and try again.')
+    print('bucket not declared. populate variables and try again.')
 
 
 print("that's all folks!!")
