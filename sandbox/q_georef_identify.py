b7307cc65f47ecd90df48abc827c7f671a828024
Marlboro Blend No. 27 <adam.breznicky@twdb.texas.gov>
2020-03-20 10:24:07 -0500

    can't use esri.

--- sandbox/q_georef_identify.py
+++ sandbox/q_georef_identify.py
@@ -10,6 +10,7 @@ if q_bucket != '' and ls4_bucket != '':
     ls4_deets = []
     q_index_keys = []
     q_tifs = []
+    coll_list = []
 
     def ls4_run(ct=None, loop=0):
         print('loop: ' + str(loop))
@@ -41,6 +42,8 @@ if q_bucket != '' and ls4_bucket != '':
                     ls4_deets.append([county, agency, year, sheet, dpi])
                 except:
                     ls4_deets.append([county, agency, year, sheet])
+                if [county, agency, year] not in coll_list:
+                    coll_list.append([county, agency, year])
                 # print(county, agency, year, sheet)
             if c['Key'][-4:] == '.TIF' or c['Key'][-4:] == '.TIFF' or c['Key'][-4:] == '.tiff':
                 print('what the TIF???' + c['Key'])
@@ -54,47 +57,47 @@ if q_bucket != '' and ls4_bucket != '':
             print('index scans .tif key count: ' + str(len(ls4_tifs)))
             return 
 
-    def q_run(ct=None, loop=0):
-        print('loop: ' + str(loop))
-        if ct is None:
-            response = client.list_objects_v2(
-                Bucket=q_bucket,
-                Prefix='prod-historic/Historic_Images/'
-            )
-        else:
-            response = client.list_objects_v2(
-                Bucket=q_bucket,
-                Prefix='prod-historic/Historic_Images/',
-                ContinuationToken=ct
-            )
-        for c in response['Contents']:
-            if '/Index/' in c['Key']:
-                # find all tifs that are not in the MultiCounty subdirectory
-                # and are not AMS (since AMS are mainly line indexes)
-                # and are not USAF (since USAF are special cases)
-                # and are not line indexes (LI) at all
-                q_index_keys.append(c['Key'])
-                if (
-                    c['Key'][-4:] == '.tif'
-                    or c['Key'][-4:] == '.TIF'
-                    ) and 'MultiCounty' not in c['Key'] and 'AMS' not in c['Key'] and 'USAF' not in c['Key'] and '_LI_' not in c['Key']:
-                    q_tifs.append(c['Key'])
-                if c['Key'][-4:] == '.TIF' or c['Key'][-4:] == '.TIFF' or c['Key'][-4:] == '.tiff':
-                    print('what the TIF???' + c['Key'])
-                # if 'USAF' in c['Key']:
-                #     print('USAF Ugh: '+ c['Key'])
+    # def q_run(ct=None, loop=0):
+    #     print('loop: ' + str(loop))
+    #     if ct is None:
+    #         response = client.list_objects_v2(
+    #             Bucket=q_bucket,
+    #             Prefix='prod-historic/Historic_Images/'
+    #         )
+    #     else:
+    #         response = client.list_objects_v2(
+    #             Bucket=q_bucket,
+    #             Prefix='prod-historic/Historic_Images/',
+    #             ContinuationToken=ct
+    #         )
+    #     for c in response['Contents']:
+    #         if '/Index/' in c['Key']:
+    #             # find all tifs that are not in the MultiCounty subdirectory
+    #             # and are not AMS (since AMS are mainly line indexes)
+    #             # and are not USAF (since USAF are special cases)
+    #             # and are not line indexes (LI) at all
+    #             q_index_keys.append(c['Key'])
+    #             if (
+    #                 c['Key'][-4:] == '.tif'
+    #                 or c['Key'][-4:] == '.TIF'
+    #                 ) and 'MultiCounty' not in c['Key'] and 'AMS' not in c['Key'] and 'USAF' not in c['Key'] and '_LI_' not in c['Key']:
+    #                 q_tifs.append(c['Key'])
+    #             if c['Key'][-4:] == '.TIF' or c['Key'][-4:] == '.TIFF' or c['Key'][-4:] == '.tiff':
+    #                 print('what the TIF???' + c['Key'])
+    #             # if 'USAF' in c['Key']:
+    #             #     print('USAF Ugh: '+ c['Key'])
                 
-        loop += 1
-        if response['IsTruncated'] is True:
-            q_run(response['NextContinuationToken'], loop)
-        else:
-            print('index file .tif key count: ' + str(len(q_tifs)))
-            return 
+    #     loop += 1
+    #     if response['IsTruncated'] is True:
+    #         q_run(response['NextContinuationToken'], loop)
+    #     else:
+    #         print('index file .tif key count: ' + str(len(q_tifs)))
+    #         return 
 
     print('compiling LS4 drive contents...')
     ls4_run()
-    print('compiling Q drive contents...')
-    q_run()
+    # print('compiling Q drive contents...')
+    # q_run()
     
     print('totals---')
     print('ls4: %s --- q: %s' % (str(len(ls4_tifs)), str(len(q_tifs))))
@@ -132,49 +135,51 @@ if q_bucket != '' and ls4_bucket != '':
     af = []
     ov = []
     counter = 1
-    for d in ls4_deets:
+    for idx, d in enumerate(coll_list):
+        print(d, idx, counter)
         # print('%s/%s' % (str(counter), str(len(ls4_deets))))
-        try:
-            keypath = 'prod-historic/Historic_Images/%s/Index/%s_%s_%s_%s.tif' % (d[0], d[1], d[2], d[3], d[4])
-        except:
-            keypath = 'prod-historic/Historic_Images/%s/Index/%s_%s_%s.tif' % (d[0], d[1], d[2], d[3])
+        # try:
+        #     keypath = 'prod-historic/Historic_Images/%s/Index/%s_%s_%s_%s.tif' % (d[0], d[1], d[2], d[3], d[4])
+        # except:
+        #     keypath = 'prod-historic/Historic_Images/%s/Index/%s_%s_%s.tif' % (d[0], d[1], d[2], d[3])
         
-        flag = False
-        if keypath not in q_index_keys:
-            flag = True
-            print('missing tif keypath: ' + keypath)
+        # flag = False
+        # if keypath not in q_index_keys:
+        #     flag = True
+        #     print('missing tif keypath: ' + keypath)
 
-        worldfile = keypath.replace('.tif', '.tfwx')
-        tfw = keypath.replace('.tif', '.tfw')
-        if worldfile not in q_index_keys:
-            if tfw not in q_index_keys:
-                print('missing worldfile: ' + tfw)
-                wf.append(keypath)
-                flag = True
-        else:
-            auxfile = keypath + '.aux.xml'
-            if auxfile not in q_index_keys:
-                print('missing auxfile: ' + auxfile)
-                af.append(keypath)
-                flag = True
+        # worldfile = keypath.replace('.tif', '.tfwx')
+        # tfw = keypath.replace('.tif', '.tfw')
+        # if worldfile not in q_index_keys:
+        #     if tfw not in q_index_keys:
+        #         print('missing worldfile: ' + tfw)
+        #         wf.append(keypath)
+        #         flag = True
+        # else:
+        #     auxfile = keypath + '.aux.xml'
+        #     if auxfile not in q_index_keys:
+        #         print('missing auxfile: ' + auxfile)
+        #         af.append(keypath)
+        #         flag = True
 
-        overviews = keypath + '.ovr'
-        if overviews not in q_index_keys:
-            print('missing overviews: ' + overviews)
-            ov.append(keypath)
-            flag = True
+        # overviews = keypath + '.ovr'
+        # if overviews not in q_index_keys:
+        #     print('missing overviews: ' + overviews)
+        #     ov.append(keypath)
+        #     flag = True
 
-        if flag:
-            ls4_not_georef.append(d)
+        # if flag:
+        #     ls4_not_georef.append(d)
 
         counter += 1
-    print('LS4 keys missing Q georef totals---')
-    print('worldfiles/auxfiles/overviews')
-    print('%s/%s/%s' % (str(len(wf)), str(len(af)), str(len(ov))))
-    print('ls4 scanned indexes missing 1(or more) georeference files in q---')
-    print('total count: %s' % str(len(ls4_not_georef)))
-    for l in ls4_not_georef:
-        print(l)
+    # print('LS4 keys missing Q georef totals---')
+    # print('worldfiles/auxfiles/overviews')
+    # print('%s/%s/%s' % (str(len(wf)), str(len(af)), str(len(ov))))
+    # print('ls4 scanned indexes missing 1(or more) georeference files in q---')
+    # print('total count: %s' % str(len(ls4_not_georef)))
+    # for l in ls4_not_georef:
+    #     print(l)
+    print('total', str(len(coll_list)))
 
 else:
     print('bucket not declared. populate variables and try again.')
